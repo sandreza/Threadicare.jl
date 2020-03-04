5fc14c3c07002ea8d30936205f98c355b3288028
sandreza <andrenogueirasouza@gmail.com>
2020-03-04 17:48:37 -0500

    documentation and package

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,11 @@
-using Documenter
+using Documenter, Threadicare
 push!(LOAD_PATH,"../src/")
+
+format = Documenter.HTML(
+    collapselevel = 1,
+       prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://sandreza.github.io/Threadicare.jl/latest/"
+)
+
 makedocs(sitename="Documentation")
+deploydocs(repo = "github.com/sandreza/Threadicare.jl.git")
