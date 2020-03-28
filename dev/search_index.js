bbd63f1af0b033e0150cf541bfaca4c8b9bffe18
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-28 03:09:05 +0000

    build based on e1b3590

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Threadicare-1","page":"Home","title":"Threadicare","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Threadicare.jl is a package that uses KernelAbstraction.jl for testing performance and creating examples","category":"page"}]
+}
